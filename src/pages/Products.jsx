783f5fe3ea89b1fc1d9b3c8b3161c2df237f65e9
unknown <amirmousazadeh1984@gmail.com>
2024-12-17 15:03:03 +0330

    chenge in padding

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,14 +1,10 @@
 import ProductList from "../features/products/ProductList";
 import styles from "./Products.module.css";
-import CreateCar from "../features/products/CreateCar";
-// import { useState } from "react";
 
 function Products() {
   return (
     <div className={styles.products}>
       <ProductList />
-
-      {/* <CreateCar /> */}
     </div>
   );
 }
